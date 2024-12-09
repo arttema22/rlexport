cbad7fa9a95de7a8275f64a8228c891b0698c82d
arttema <arttema@mail.ru>
2024-12-09 13:58:33 +0300

    fix: edit salary page

--- app/Livewire/Salary/SalaryPage.php
+++ app/Livewire/Salary/SalaryPage.php
@@ -4,18 +4,26 @@ namespace App\Livewire\Salary;
 
 use Livewire\Component;
 use App\Models\Main\Salary;
+use Livewire\WithPagination;
+use Livewire\Attributes\Lazy;
+use Livewire\WithoutUrlPagination;
 
+#[Lazy]
 class SalaryPage extends Component
 {
-    public function mount() {}
+    use WithPagination, WithoutUrlPagination;
 
     public function render()
     {
-        $salaries = Salary::all();
-
+        $salaries = Salary::with(['owner'])->with(['driver'])->simplePaginate(3, pageName: 'salaries');
         return view('livewire.salary.salary-page')
             ->with([
                 'salaries' => $salaries,
             ]);
+    }
+
+    public function placeholder()
+    {
+        return view('livewire.salary.spinner');
     }
 }
--- resources/views/livewire/salary/salary-page.blade.php
+++ resources/views/livewire/salary/salary-page.blade.php
@@ -1,3 +1,45 @@
-<div class="min-h-screen bg-gray-100">
-    {{$salaries}}
+<div class="grid grid-cols-1 md:grid-cols-3 gap-2">
+
+    <div class="md:col-span-2">
+        <x-blocks.list>
+            <x-slot name="title">
+                <x-blocks.title>
+                    {{__('Salaries')}}
+                </x-blocks.title>
+                <x-blocks.btn-round-new wire:click="create" title="{{__('New salary')}}" />
+            </x-slot>
+            <x-slot name="header">
+                <div class="w-1/3">{{__('Date')}}</div>
+                <div class="w-1/3">{{__('Sum')}}</div>
+                <div class="w-1/3">{{__('Comment')}}</div>
+            </x-slot>
+            <x-slot name="content">
+                @foreach ( $salaries as $salary )
+                <x-blocks.accordion>
+                    <x-blocks.btn-accordion>
+                        <div class="w-1/3">{{$salary->event_date->format(config('app.date_format'))}}</div>
+                        <div class="w-1/3">{{$salary->sum}}</div>
+                        <div class="w-1/3">{{$salary->comment}}</div>
+                    </x-blocks.btn-accordion>
+                    <x-blocks.content-accordion>
+                        <div class="mr-2 text-xs text-gray-600 leading-relaxed">
+                            {{__('Created')}} {{$salary->created_at}}
+                            {{__('Updated')}} {{$salary->updated_at}}
+                            {{__('Owner')}} {{$salary->owner->name}}
+                            {{__('Driver')}} {{$salary->driver->name}}
+                        </div>
+                        <div class="flex">
+                            <x-blocks.btn-edit wire:click="edit({{ $salary->id }})" />
+                            <x-blocks.btn-delete wire:click="confirmDelete({{ $salary->id }})" />
+                        </div>
+                    </x-blocks.content-accordion>
+                </x-blocks.accordion>
+                @endforeach
+                {{$salaries->links()}}
+            </x-slot>
+        </x-blocks.list>
+    </div>
+    <div>
+        @livewire('Salary.SalaryCard')
+    </div>
 </div>
